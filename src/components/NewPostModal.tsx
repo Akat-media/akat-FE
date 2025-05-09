0ff1e5f44f16747ae906d23fc1c9c563f663ffa3
khanhpd <duykhanh141197@gmail.com>
2025-05-09 10:20:53 +0700

    edit to component loading content

--- src/components/NewPostModal.tsx
+++ src/components/NewPostModal.tsx
@@ -15,6 +15,7 @@ import {
 import axios from 'axios';
 import { BaseUrl } from '../constants';
 import { toast } from 'react-toastify';
+import LoadingContent from './content-management/post-managenment/LoadingContent.tsx';
 
 interface NewPostModalProps {
   page: any;
@@ -309,12 +310,7 @@ function NewPostModal({ page, onClose }: NewPostModalProps) {
           </button>
         </div>
 
-        {isLoading && (
-          <div className="fixed inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center z-50 space-y-4">
-            <Loader2 className="w-10 h-10 text-white animate-spin" />
-            <p className="text-white text-lg font-medium">Đang đăng bài...</p>
-          </div>
-        )}
+        {isLoading && LoadingContent()}
 
       </div>
     </div>
