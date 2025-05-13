76abb9bf2597e69b3dfc1444f004d4a6b1753529
nrphuoc <nguyentuongphuoc@gmail.com>
2025-05-13 17:48:40 +0700

    deo-8

--- src/pages/PostManagementPage.tsx
+++ src/pages/PostManagementPage.tsx
@@ -23,6 +23,7 @@ import { debounce } from 'lodash';
 import defaultImage from '../../public/default-avatar.jpg';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { io } from 'socket.io-client';
 
 const PostSchedule = lazy(() => import('../components/PostSchedule'));
 const NewPostModal = lazy(() => import('../components/NewPostModal'));
@@ -53,6 +54,8 @@ function PostManagementPage() {
   );
   const [setting, setSetting] = useState<any>(null);
   const [refresh, setRefresh] = useState<any>(null);
+
+  const [socket, setSocket] = useState<any>(null);
   const fetchPostsFromConnectedPages = async () => {
     try {
       setLoading(true);
@@ -146,6 +149,23 @@ function PostManagementPage() {
       handleCallPost();
     }
   }, [fanpages, currentPage, pageSize, selectedFanpage, search]);
+
+  // Thử nha
+  useEffect(() => {
+    // Kết nối tới server Socket.IO
+    const socketInstance = io('http://localhost:3000'); // Thay bằng URL server của bạn
+    setSocket(socketInstance);
+
+    // Lắng nghe sự kiện từ server
+    socketInstance.on('notification', (message: string) => {
+      toast.info(message); // Hiển thị thông báo
+    });
+
+    // Ngắt kết nối khi component bị unmount
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
 
   const { innerBorderRef } = useOnOutsideClick(() => setShowFanpageDropdown(false));
   function getFirstImage(input: any) {
